f7e097f9d31c7d9a475c522513e04534ec23138f
rauldangio <jojofagqwrt@gmail.com>
2023-03-25 07:11:26 -0300

    criando subclasses Player e Inimigo

--- pessoa.py
+++ pessoa.py
@@ -1,2 +1,26 @@
+from pokemon import PokemonGrama
+
 class Pessoa():
-    pass
+    def __init__(self,nome="Anonimo",pokemons=[]):
+        self.nome = nome
+        self.pokemons = pokemons
+    def __str__(self):
+        return f"{self.nome}"
+
+    def mostrar_pokemons(self):
+        for pokemon in self.pokemons:
+            print(pokemon.__str__())
+
+
+class Player(Pessoa):
+    tipo = "player"
+
+
+class nimigo(Pessoa):
+    tipo = "inimigo"
+  
+    
+pokemon_do_raul = PokemonGrama("Bulbassauro",level=5)
+eu = Player("Raul",[pokemon_do_raul])
+print(eu)
+eu.mostrar_pokemons()
