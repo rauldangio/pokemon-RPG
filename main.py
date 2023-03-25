0f99458e497e70d9a6caa84a99123998f08911be
rauldangio <jojofagqwrt@gmail.com>
2023-03-25 13:55:45 -0300

    criando o metodo escolher_nome_jogador

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from pessoa import *
+from time import sleep
 
+def escolhendo_nome_jogador():
+    print(f"Olá, Bem vindo ao mundo de Pokemon")
+    sleep(1)
+    print(f"qual era mesmo o seu nome?")
+    sleep(1)
+
+    for i, nome in enumerate(NOMES):
+        print(f"{i} --> {nome}")
+        sleep(0.2)
+    print(f'{len(NOMES)} --> outro')
+    nome = int(input("Jogador: Meu nome é "))
+    if nome == 0:
+        return NOMES[0]
+    if nome == 1:
+        return NOMES[1]
+    if nome == 2:
+        return NOMES[2]
+    if nome == 3:
+        return NOMES[3]
+    if nome == 4:
+        return NOMES[4]
+    if nome == 5:
+        nome = input("Digite seu nome:")
+        print("Que nome bonito!")
+        return nome
+    
 def escolher_pokemon_inicial(player):
-    print(f"Olá {player}, Bem vindo ao mundo de Pokemon")
+    print(f"então seu nome é {player}")
     print("Sinta-se livre para capturar seu primeiro pokemon")
     
     while True:
@@ -19,4 +46,5 @@ def escolher_pokemon_inicial(player):
         else:
             print("****por favor escolha do numero de 1 a tres****\n")
     
-escolher_pokemon_inicial(Player())
+
+escolher_pokemon_inicial(Player(escolhendo_nome_jogador()))
