1cb0e0860f1c6000e9c2a61faadb7e62bb8f0402
rauldangio <jojofagqwrt@gmail.com>
2023-03-25 06:03:26 -0300

    criando metodo __init__

--- pokemon.py
+++ pokemon.py
@@ -1,2 +1,8 @@
 class Pokemon:
-    pass
+    def __init__(self, tipo, nome): # toda vez que instanciar um objeto essa metodo vai ser usada/construtor
+        self.tipo = tipo
+        self.nome = nome
+
+meu_pokemon = Pokemon("fogo","charmander")
+pokemon_do_joao_erik = Pokemon("eletrico","pikachu")
+print(meu_pokemon.tipo)
