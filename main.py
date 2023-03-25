ab60172559abee98b0af5323df25a02e6ea2900a
rauldangio <jojofagqwrt@gmail.com>
2023-03-25 13:41:15 -0300

    melhorando a funcao inicial

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ from pessoa import *
 def escolher_pokemon_inicial(player):
     print(f"Olá {player}, Bem vindo ao mundo de Pokemon")
     print("Sinta-se livre para capturar seu primeiro pokemon")
-    print("1 --> Bulbassauro\n2 --> Charmander\n3 --> Squirtle")
-    pokemon_inicial = int(input("Digite qual pokemon você deseja: "))
-    if pokemon_inicial == 1:
-        player.capturar_100(PokemonGrama("Bulbassauro"))
-    elif pokemon_inicial == 2:
-        player.capturar_100(PokemonFogo("Charmander"))
-    elif pokemon_inicial == 3:
-        player.capturar_100(PokemonAgua("Squirtle"))
-
+    
+    while True:
+        print("1 --> Bulbassauro\n2 --> Charmander\n3 --> Squirtle")
+        pokemon_inicial = int(input("Digite qual pokemon você deseja: "))
+        if pokemon_inicial == 1:
+            player.capturar_100(PokemonGrama("Bulbassauro"))
+            break
+        elif pokemon_inicial == 2:
+            player.capturar_100(PokemonFogo("Charmander"))
+            break
+        elif pokemon_inicial == 3:
+            player.capturar_100(PokemonAgua("Squirtle"))
+            break
+        else:
+            print("****por favor escolha do numero de 1 a tres****\n")
+    
 escolher_pokemon_inicial(Player())
