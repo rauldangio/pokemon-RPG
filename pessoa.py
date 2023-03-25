50bd98b3091f5ab324f5a680259e4e48f7d0a123
rauldangio <jojofagqwrt@gmail.com>
2023-03-25 12:39:04 -0300

    randomizando os inimigos

--- pessoa.py
+++ pessoa.py
@@ -3,20 +3,33 @@ import random
 
 NOMES = ["joao","raul","cre","RRR","gary"]
 
+POKEMONS = [
+    PokemonAgua("Squirtle"),
+    PokemonGrama("Bulbassauru"),
+    PokemonFogo("Charmander")
+]
+
 class Pessoa():
-    def __init__(self,nome="Anonimo",pokemons=[]):
-        self.nome = nome
+    def __init__(self,nome = None,pokemons=[]):
+        if nome:
+            self.nome = nome
+        else:
+            self.nome = NOMES[random.randint(0,len(NOMES)-1)]
         self.pokemons = pokemons
+
+
     def __str__(self):
         return f"{self.nome}"
+
 
     def mostrar_pokemons(self):
         if self.pokemons:
             print(f"{self} tem os seguintes pokemons:")
             for pokemon in self.pokemons:
-                print(f"-->{pokemon}")
+                print(f"-->{pokemon} --> {pokemon.especie}")
         else:
             print(f"{self} Nao tem pokemon!")
+
 
 class Player(Pessoa):
     tipo = "player"
@@ -28,12 +41,16 @@ class Player(Pessoa):
         else:
             print(f"{self} não conseguiu capturar o {pokemon}!")
 
-class nimigo(Pessoa):
-    tipo = "inimigo"
-  
-    
 
-eu = Player("Raul")
-eu.mostrar_pokemons()
-eu.capturar(PokemonFogo("charmander"))
-eu.mostrar_pokemons()
+class Inimigo(Pessoa):
+    tipo = "inimigo"
+    
+    def __init__(self, nome=None, pokemons=[]):
+        super().__init__(nome, pokemons)   
+        if not pokemons:
+            for i in range (6):
+                self.pokemons.append(random.choice(POKEMONS))
+    
+gary = Inimigo()
+print(gary)
+gary.mostrar_pokemons()
