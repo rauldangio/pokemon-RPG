19cfdfdb7ece176e8be682cd6dd907bd2127478b
rauldangio <jojofagqwrt@gmail.com>
2023-03-25 06:56:41 -0300

    criando outras subclasses

--- pokemon.py
+++ pokemon.py
@@ -1,6 +1,5 @@
 class Pokemon:
-    def __init__(self, tipo, especie, level=1, nome=None, ): # toda vez que instanciar um objeto essa metodo vai ser usada/construtor
-        self.tipo = tipo
+    def __init__(self,especie, level=1, nome=None, ): # toda vez que instanciar um objeto essa metodo vai ser usada/construtor
         self.especie = especie
         if nome:
             self.nome = nome
@@ -16,16 +15,47 @@ class Pokemon:
 
 
 class PokemonEletrico(Pokemon): # classe filha do Pokemon
+    tipo = "eletrico"
+
     def atacar(self,pokemon):
         print(f"{self} usou choque do trovao em {pokemon}!") # sobreescrevendo o metodo atacar (overwrite)
 
     def dar_choque(self):
         print(f"{self} deu choque")
 
-pokemon = PokemonEletrico("eletrico","pikachu")
-pokemon_do_joao_erik = Pokemon("fogo","charmander")
 
-pokemon.atacar(pokemon_do_joao_erik)
-pokemon_do_joao_erik.atacar(pokemon)
+class PokemonFogo(Pokemon): # classe filha do Pokemon
+    tipo = "fogo"
+    
+    def atacar(self,pokemon):
+        print(f"{self} usou firethrower em {pokemon}!") # sobreescrevendo o metodo atacar (overwrite)
 
-pokemon.dar_choque()
+    def fogoPelaBoca(self):
+        print(f"{self} Soltou fogo pela boca")
+
+
+class PokemonAgua(Pokemon): # classe filha do Pokemon
+    tipo = "agua"
+    
+    def atacar(self,pokemon):
+        print(f"{self} usou watergun em {pokemon}!") # sobreescrevendo o metodo atacar (overwrite)
+
+    def cuspiu(self):
+        print(f"{self} cuspiu!")
+
+
+class PokemonGrama(Pokemon): # classe filha do Pokemon
+    tipo = "grama"
+    
+    def atacar(self,pokemon):
+        print(f"{self} usou lifeseed em {pokemon}!") # sobreescrevendo o metodo atacar (overwrite)
+
+    def plantouTrigo(self):
+        print(f"{self} plantou trigo")
+
+meu_pokemon = PokemonAgua("squirtle")
+vinicius_pokemon = PokemonFogo("charmander")
+
+print(meu_pokemon)
+
+
