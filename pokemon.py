e10d140c12d8755c4b5233703187dc698fdd22e9
rauldangio <jojofagqwrt@gmail.com>
2023-03-25 06:09:53 -0300

    criando metodo str

--- pokemon.py
+++ pokemon.py
@@ -2,7 +2,10 @@ class Pokemon:
     def __init__(self, tipo, nome): # toda vez que instanciar um objeto essa metodo vai ser usada/construtor
         self.tipo = tipo
         self.nome = nome
+    
+    def __str__(self):
+        return f"Tipo: {self.tipo}\nEspecie: {self.nome}"
 
 meu_pokemon = Pokemon("fogo","charmander")
 pokemon_do_joao_erik = Pokemon("eletrico","pikachu")
-print(meu_pokemon.tipo)
+print(meu_pokemon)
